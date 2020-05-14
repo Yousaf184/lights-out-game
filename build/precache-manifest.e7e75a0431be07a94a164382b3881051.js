06e5d5333d9943f7beb61f86cb5c730ae3181f02
Yousaf Khan <khanyousaf184@gmail.com>
2020-05-14 22:47:14 +0500

    created a nodejs server to serve production build

---
+++ build/precache-manifest.e7e75a0431be07a94a164382b3881051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1521eaec66f467e8f2cbcdc937dae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a567bb4a2d211cf249",
+    "url": "/static/css/main.806e4d43.chunk.css"
+  },
+  {
+    "revision": "0dcb8dc903d440c22ceb",
+    "url": "/static/js/2.27921631.chunk.js"
+  },
+  {
+    "revision": "68a567bb4a2d211cf249",
+    "url": "/static/js/main.bb114bba.chunk.js"
+  },
+  {
+    "revision": "440fb12e9ba7e9b6cc55",
+    "url": "/static/js/runtime~main.f0e66640.js"
+  }
+]);
